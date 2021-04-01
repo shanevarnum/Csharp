35d472b8caaeb7a267f7c24976d149abd43e4db6
shanevarnum <80869221+shanevarnum@users.noreply.github.com>
2021-04-01 19:07:25 -0400

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemographicAnalyticalReportsVarn
+{
+    public partial class FormReports : Form
+    {
+        public FormReports()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonExitReports_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void FormReports_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'adventureWorks2012_DataDataSet.vPersonDemographics' table. You can move, or remove it, as needed.
+            this.vPersonDemographicsTableAdapter.Fill(this.adventureWorks2012_DataDataSet.vPersonDemographics);
+
+        }
+
+        private void demographicReportOptionsToolStripButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.vPersonDemographicsTableAdapter.DemographicReportOptions(this.adventureWorks2012_DataDataSet.vPersonDemographics, maritalStatusToolStripTextBox.Text, genderToolStripTextBox.Text, new System.Nullable<int>(((int)(System.Convert.ChangeType(totalChildrenToolStripTextBox.Text, typeof(int))))), educationToolStripTextBox.Text, occupationToolStripTextBox.Text);
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
